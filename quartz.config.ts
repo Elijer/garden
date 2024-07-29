2c2ce30ec5eb9235d6dae1f6f3f83cff3bd1a2b6
Elijah Kennedy <elijahclimbs@gmail.com>
2024-07-29 18:49:32 -0400

    messed with date configuration

--- quartz.config.ts
+++ quartz.config.ts
@@ -36,7 +36,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "elijer.github.io/garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
@@ -116,7 +116,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
+        priority: ["frontmatter", "git", "filesystem"],
       }),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting({
