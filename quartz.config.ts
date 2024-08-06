d3b79df89a492d42b288ebbe53ce941fbcf8a297
Elijah Kennedy <elijahclimbs@gmail.com>
2024-08-06 00:31:23 -0400

    Quartz sync: Aug 6, 2024, 12:31 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Elijah.Kennedy",
+    pageTitle: "Elijah Kennedy",
     enableSPA: true,
     enablePopovers: true,
     analytics: [
